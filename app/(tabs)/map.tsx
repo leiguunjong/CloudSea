be2ad05e64144163f0ac5f5d93fc396806431c3a
leiguunjong <1245468434@qq.com>
2026-05-20 13:05:52 +0800

    map

--- app/(tabs)/map.tsx
+++ app/(tabs)/map.tsx
@@ -1,9 +1,25 @@
-import { View, Text } from "react-native";
+import { View, StyleSheet } from "react-native";
+import { MapView } from "expo-gaode-map";
 
 export default function MapPage() {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>地图页面</Text>
+    <View style={styles.container}>
+      <MapView
+        style={styles.map}
+        initialCameraPosition={{
+          target: {
+            latitude: 39.91095,
+            longitude: 116.37296,
+          },
+          zoom: 11,
+        }}
+        myLocationEnabled
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: { flex: 1 },
+  map: { flex: 1 },
+});
